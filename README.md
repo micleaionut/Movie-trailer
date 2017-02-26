ed0866fba483564a0931358206907110800e3be8
Catalin Ionut Miclea <gen_catalin@yahoo.com>
2017-02-26 21:34:13 +0100

    New submission

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ud036_StarterCode
-Source code for a Movie Trailer website.
+To run the program you have to open entertainment_center.py and Run (F5).
+This will send the paramaters with movies details to fresh_tomatoes.html which init the Movie class from media.py
--- media.py
+++ media.py
@@ -1,9 +1,11 @@
+# import module
 import webbrowser
 
+
+# create class Movie
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_story, poster_img, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
+        self.storyline = movie_story
+        self.poster_image_url = poster_img
         self.trailer_youtube_url = trailer_youtube
-        
